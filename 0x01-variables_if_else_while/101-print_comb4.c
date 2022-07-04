182773f69db7bd4d9b774c018285825223af7a32
kirub-getch <chaoskirub@gmail.com>
2022-07-04 08:43:24 -0700

    correction for task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,11 +12,14 @@ for (tens = 0 ; tens <= 9; tens++)
 {
 for (ones = tens + 1; ones <= 9; ones++)
 {
-putchar("%d%d%d", hund, tens, ones);
+putchar(hund + '0');
+putchar(tens + '0');
+putchar(ones + '0');
 putchar(',');
 putchar(' ');
 }
 }
 }
+putchar('\n');
 return (0);
 }
