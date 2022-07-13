809f3d34c482f471f151dabb60482cba28070fca
kirub-getch <chaoskirub@gmail.com>
2022-07-13 06:09:16 -0700

    correction

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,33 +6,28 @@
 */
 int main(void)
 {
-int i ,j;
-for(i = 0 ;i <= 99 ;i++)
+int i, j;
+for (i = 0; i <= 99; i++)
 {
-for(j =0;j<= 99 ; j++)
+for (j = i ; j <= 99; j++)
 {
-if(i != j)
+if (i != j)
 {
-putchar(i % 10 + 48);
-putchar(j % 10 + 48);
-putchar(' ');
 putchar(i / 10 + 48);
+putchar(i % 10 + 48);
+putchar(' ');
 putchar(j / 10 + 48);
-	
-if((i % 10 == 9 && j % 10 == 8 ) && (i / 10 == 9 && j / 10 == 9))
+putchar(j % 10 + 48);	
+if ((i / 10 == 9 && j % 10 == 9 ) && (i % 10 == 8 && j / 10 == 9))
 {
 break;
 }
 
 putchar(' ');
 putchar(',');	
-
 }
 }
 putchar('\n');
 }
-
-
-
 return (0);
 }
