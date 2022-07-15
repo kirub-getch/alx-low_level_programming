bc89109de65aca7226491eb5baa3987b3b744c9c
kirub-getch <chaoskirub@gmail.com>
2022-07-15 13:00:29 -0700

    corection

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,10 +9,10 @@ void print_number(int n)
 unsigned int num = n;
 if (x < 0)
 {
-putchar('-');
+_putchar('-');
 num = -num;
 }
 if((num / 10) > 0)
 print_number(num / 10);
-putchar((num % 10) + '0');
+_putchar((num % 10) + '0');
 }
