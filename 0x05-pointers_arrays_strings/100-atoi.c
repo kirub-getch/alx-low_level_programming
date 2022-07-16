301a70f49763176bc56246f5efba361cd89079d7
kirub-getch <chaoskirub@gmail.com>
2022-07-16 06:50:37 -0700

    2nd corection for task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**********************************************
 * _atoi - converts strings to an integer.
-* 
+*
 * @s: pointer to a character string.
 *
 * Return: void
 *
 ***********************************************/
-int _atoi(char *s)
+int _atio(char *s)
 {
 int sign;
 unsigned int num;
@@ -16,19 +16,21 @@ char *temp;
 temp = s;
 num = 0;
 sign = 1;
-while(*temp != '/0' && (*temp < '0' || *temp > '9')
+
+while(*temp != '\0' && (*temp < '0' || *temp > '9' ))
 {
 if(*temp == '-')
-sign *= -1;
+ sign *= -1;
 temp++;
 }
-if(*temp != '\0')
+if (*temp != '\0')
 {
-do{
+do
+{
 num = num * 10 + (*temp - '0');
-temp++;
 }
-while (*temp >= '0' && *temp <= '9');
+while
+(*temp >= '0' && *temp <= '9');
 }
-return (num * sign);
+return ( num * sign);
 }
