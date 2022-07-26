3c94c7edc747aac9d2f60236c6f63494d6179d24
kirub-getch <chaoskirub@gmail.com>
2022-07-26 13:34:18 -0700

    correction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,42 +8,31 @@
  **/
 char *str_concat(char *s1, char *s2)
 {
-char *strDup;
-int x, z;
-if (s1 == NULL)
-{
-s1 = "";
-}
-if (s2 == NULL)
-{
-s2 = "";
-}
-x = z = 0;
-while (s1[x] != '\0')
-{
-x++;
-}
-z = 0;
-while (s2[z] != '\0')
-{
-z++;
-}
-strDup = malloc(sizeof(char) * (x + z + 1));
-if (strDup == NULL)
-{
-return (NULL);
-x = z = 0;
-}
-while (s1[x] != '\0')
-{
-strDup[x] = s1[x];
-x++;
-}
-while (s2[z] != '\0')
-{
-strDup[x] = s2[z];
-x++, z++;
-}
-strDup[x] = '\0';
-return (strDup);
+	char *strDup;
+	int i, j;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	i = j = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+	strDup = malloc(sizeof(char) * (i + j + 1));
+	if (strDup == NULL)
+		return (NULL);
+	i = j = 0;
+	while (s1[i] != '\0')
+	{
+		strDup[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		strDup[i] = s2[j];
+		i++, j++;
+	}
+	strDup[i] = '\0';
+	return (strDup);
 }
