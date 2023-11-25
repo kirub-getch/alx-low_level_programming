045a3b39840c02e32980a575d0320a476b9a5962
kirub-getch <chaoskirub@gmail.com>
2023-11-25 17:09:13 +0300

    fixed code

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,10 +14,10 @@ unsigned int binary_to_uint(const char *b)
 	if (*b == '\0')
 		return (0);
 
-	for (len = 0; b[len];)
+	while(b[len])
 		len++;
 
-	for (len -= 1; len >= 0; len--)
+	for (len = 1; len >= 0; len--)
 	{
 		if (b[len] != '0' && b[len] != '1')
 			return (0);
