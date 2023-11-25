254ce71a83174a05d25d6ff4306f2cdc733c4e50
kirub-getch <chaoskirub@gmail.com>
2023-11-25 20:01:30 +0300

    fixed error incode

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * binary_to_uint - Converts a binary number to an unsigned int.
  * @b: A pointer to a string of 0 and 1 chars.
@@ -7,12 +6,15 @@
  * Return: If b is NULL or contains chars not 0 or 1 - 0.
  *         Otherwise - the converted number.
  */
-unsigned int binary_to_uint(const char *b) {
+unsigned int binary_to_uint(const char *b)
+ {
     if (b == NULL)
         return 0;
 
     unsigned int result = 0;
-    while (*b != '\0') {
+	
+    while (*b != '\0')
+	 {
         if (*b != '0' && *b != '1')
             return 0; // Return 0 if the string contains characters other than '0' or '1'
         
